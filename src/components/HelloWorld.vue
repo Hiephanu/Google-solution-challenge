ba525fa7d18a5c238744c5083a5b42338d70ab99
Hiephanu <doanvanhiep230903@gmail.com>
2023-02-13 15:52:09 +0700

    Update

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-blue-600">Hi</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
